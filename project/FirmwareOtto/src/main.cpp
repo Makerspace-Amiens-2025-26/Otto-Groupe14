bf18e447c3a6042eeaa42d429e9fc29a4e6baba7
Nayhyx <nathan.baronick@etu.unilasalle.fr>
2026-02-25 15:23:56 +0100

    Add startup melody for buzzer
    Define 4th-octave note frequency constants and a DUREE_TEMPS duration, and implement startupMusic() to play a sequence of tones on BUZZER_PIN (D0) using tone()/noTone() with scaled delays. Replace the previous ad-hoc tones in setup() with a call to startupMusic(); loop() remains empty. This centralizes the startup melody and makes note values and timing configurable.

--- project/FirmwareOtto/src/main.cpp
+++ project/FirmwareOtto/src/main.cpp
@@ -1,16 +1,49 @@
 #include <Arduino.h>
 
+// Définition des fréquences des notes de musiques de la 4ème octave
+#define OCTAVE_4_DO 523
+#define OCTAVE_4_RE 587
+#define OCTAVE_4_MI 659
+#define OCTAVE_4_FA 698
+#define OCTAVE_4_SOL 784
+#define OCTAVE_4_LA 880
+#define OCTAVE_4_SI 988
+#define DUREE_TEMPS 300  // Durée de chaque note en millisecondes
+
 #define BUZZER_PIN D0  // Définition de la broche du buzzer
 
-void setup() {
-  tone(BUZZER_PIN, 500, 300);  // Émet un son à 500 Hz pendant 300 ms
-  delay(500);
-  tone(BUZZER_PIN, 1000, 300); // Émet un son à 1000 Hz pendant 300 ms
-  delay(500);
-  tone(BUZZER_PIN, 1500, 300); // Émet un son à 1500 Hz pendant 300 ms
-  delay(500);
-  noTone(BUZZER_PIN);          // Silence après la série de sons
+
+void startupMusic() { // Fonction pour jouer la musique de démarrage
+  tone(BUZZER_PIN, OCTAVE_4_DO, DUREE_TEMPS);
+  delay(DUREE_TEMPS*1.3); // Pause entre les notes
+  tone(BUZZER_PIN, OCTAVE_4_DO, DUREE_TEMPS);
+  delay(DUREE_TEMPS*1.3);
+  tone(BUZZER_PIN , OCTAVE_4_DO, DUREE_TEMPS);
+  delay(DUREE_TEMPS*1.3);
+  tone(BUZZER_PIN, OCTAVE_4_RE, DUREE_TEMPS);
+  delay(DUREE_TEMPS*1.3);
+  tone(BUZZER_PIN, OCTAVE_4_MI, 2 * DUREE_TEMPS);
+  delay(2.6 * DUREE_TEMPS);
+  tone(BUZZER_PIN, OCTAVE_4_RE, 2 * DUREE_TEMPS);
+  delay(2.6 * DUREE_TEMPS);
+  tone(BUZZER_PIN, OCTAVE_4_DO, DUREE_TEMPS);
+  delay(DUREE_TEMPS*1.3);
+  tone(BUZZER_PIN, OCTAVE_4_MI, DUREE_TEMPS);
+  delay(DUREE_TEMPS*1.3);
+  tone(BUZZER_PIN, OCTAVE_4_RE, DUREE_TEMPS);
+  delay(DUREE_TEMPS*1.3);
+  tone(BUZZER_PIN, OCTAVE_4_RE, DUREE_TEMPS);
+  delay(DUREE_TEMPS*1.3);
+  tone(BUZZER_PIN, OCTAVE_4_DO, 4 * DUREE_TEMPS);
+  delay(5.2 * DUREE_TEMPS);
+  noTone(BUZZER_PIN);    // Silence après la série de sons
 }
+
+
+void setup() { // Fonction d'initialisation de la carte
+  startupMusic();
+}
+
 
 void loop() {
   // Rien à faire dans la boucle principale
